d625cf8422552e47f177fb4c77b77369f988e773
perebaj <perebaj@gmail.com>
2019-09-26 15:25:42 -0300

    Nem precisa de tudo isso

--- main.cpp
+++ main.cpp
@@ -1,12 +1,60 @@
-#include "interpreter.h"
+#include <map>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <iterator>
+#include <boost/algorithm/string/join.hpp>
+#include <sstream>
 
-int main(int argc, char**argv){
-    string in;
 
-    if(argc > 1)
-        in = "cin";
+using namespace std;
 
-    interpreter inter(in);
-    inter.exec();
+bool interpreter(string argzero){
+    if(argzero.compare("I") == 0){
+        cout << "Inserção" << endl;
+        return true;
+    }
+    else if(argzero.compare("R") == 0){
+        cout << "Remoção" << endl;
+        return true;
+    }
+    else if(argzero.compare("A") == 0){
+        cout << "Alteração" << endl;
+        return true;
+    }
+    else if (argzero.compare("S") == 0){
+        cout << "Sainda" << endl;
+        return false;
+    }
+    else return false;
+}
+
+vector<string> splitted(){
+    string input;
+    vector<string> splitted_input;
+
+    getline(cin, input);
+
+    stringstream iss(input);
+    if (input.compare("") != 0)
+    {
+        for (string s; iss >> input;)
+            splitted_input.push_back(input);
+        transform(splitted_input[0].begin(), splitted_input[0].end(), splitted_input[0].begin(), ::toupper);
+    }
+    return splitted_input;
+}
+
+
+int main()
+{
+    vector<string> input;
+    bool flag;
+    do{
+        input = splitted();
+        flag = interpreter(input[0]);
+        
+    }while(flag);
+
     return 0;
 }
--- makefile
+++ makefile
@@ -1,17 +1,15 @@
-all: littlecontato
+all: schagend
+
 
 CFLAGS = -W -m64 -Wall -std=c++1z
-littlecontato: makedir/main.o makedir/interpreter.o makedir/worker.o
-	g++ -o littlecontato makedir/main.o  makedir/worker.o makedir/interpreter.o $(CFLAGS)
+
+schagend: makedir/main.o
+	g++ -o schagend makedir/main.o $(CFLAGS)
 makedir/main.o: main.cpp
 	g++ -o makedir/main.o -c main.cpp $(CFLAGS)
-makedir/interpreter.o: interpreter.cpp
-	g++ -o makedir/interpreter.o -c interpreter.cpp $(CFLAGS)
-makedir/worker.o: worker.cpp
-	g++ -o makedir/worker.o -c worker.cpp $(CFLAGS)
 
 
 clean:
 	rm -rf makedir/*.o
-mrproper: clean
-	rm -rm littlecontato
+mproper: clean
+	rm -rm schagend
